8b25be2d2237699e5e069895d525f2dc9379b5d7
0w0 <kamata.aya2019@gmail.com>
2025-01-27 14:05:54 +0100

    refactor snippets and update cpptest.ps1

--- script/cpptest.ps1
+++ script/cpptest.ps1
@@ -1,9 +1,14 @@
 $problem_name = $Args[0]
-$base_url = ($problem_name.Replace("_", "-")).Substring(0, ($problem_name.Length) - 2)
 $test_dir = "test/$problem_name"
 
-# 問題のURLを生成
-$url = "https://atcoder.jp/contests/$base_url/tasks/$problem_name"
+if ($problem_name.Substring(0, 5) -eq "ALDS1") {
+  $url = "https://onlinejudge.u-aizu.ac.jp/problems/$problem_name"
+} else {
+  $base_url = $problem_name.Replace("_", "-").Substring(0, $problem_name.Length - 2)
+
+  # 問題のURLを生成
+  $url = "https://atcoder.jp/contests/$base_url/tasks/$problem_name"
+}
 
 # テストケースのディレクトリが存在しなければ作成し、テストケースをダウンロード
 if (! (Test-Path $test_dir)) {
@@ -12,5 +17,9 @@ if (! (Test-Path $test_dir)) {
 
 
 # コンパイルとテスト実行
-g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && oj s $url src/$problem_name.cpp && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
+if ($problem_name.Substring(0, 5) -eq "ALDS1"){
+  g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
+}else{
+  g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && oj s $url src/$problem_name.cpp && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
+}
 Remove-Item -Force -Path bin/* -ErrorAction SilentlyContinue
