5bd42da42b7f7b434980331a0ef38f3eee6c84f0
0w0 <kamata.aya2019@gmail.com>
2025-02-10 01:25:32 +0100

    practice normal dp

---
+++ src/2199.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rrep(i, a, b) for (ll i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+#pragma GCC optimize("-O3")
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using i128 = __int128_t;
+using pll = pair<ll, ll>;
+using pld = pair<ld, ld>;
+using matrix = vector<vector<ll>>;
+using matrixld = vector<vector<ld>>;
+using matrixb = vector<vector<bool>>;
+using onevec = vector<ll>;
+using onevecld = vector<ld>;
+using onevecb = vector<bool>;
+using lset = set<ll>;
+
+const ll INFL = 1LL << 60;
+template <class T> bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T> bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T> ll lsize(const T &container) {
+  return static_cast<ll>(container.size());
+}
+template <typename T> bool ifbit(T number, ll bit) {
+  return (number & (1LL << bit)) != 0;
+}
+template <typename T> ll twice(const T &a) { return a * a; }
+template <typename T> T bits_count(T v) { return __builtin_popcountll(v); }
+
+template <typename T> T factorial(T n) {
+  T result = 1;
+  for (T i = 1; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
+}
+
+template <typename T> T pdistance(const vector<pair<T, T>> &xy, int i, int j) {
+  return sqrt(twice(xy[i].first - xy[j].first) +
+              twice(xy[i].second - xy[j].second));
+}
+
+template <typename T> void print(T value, int precision) {
+  cout << fixed << setprecision(precision) << value << endl;
+}
+
+template <typename... Args> void in(Args &...args) { (cin >> ... >> args); }
+
+template <typename... Args> void out(const Args &...args) {
+  constexpr size_t n = sizeof...(args);
+  size_t i = 0;
+  ((cout << args << (++i < n ? " " : "")), ...) << "\n";
+}
+
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, 1, -1, 1, -1};
+string strrep(const string &s, const string &from, const string &to) {
+
+  unordered_map<char, char> trans;
+  for (size_t i = 0; i < from.size(); ++i) {
+    trans[from[i]] = to[i];
+  }
+
+  string res = s;
+  for (char &c : res) {
+    if (trans.count(c)) {
+      c = trans[c];
+    }
+  }
+  return res;
+}
+/*--------------------------------------------------------
+                         \0w0/
+                        OwOkaomoji
+                     ｡˚ (¦3ꇤ )3 ⋆｡˚✩
+----------------------------------------------------------*/
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  while (true) {
+    ll n, m;
+    cin >> n >> m;
+    if (n == 0 && m == 0)
+      break;
+
+    onevec c(m);
+    rep(i, 0, m) in(c[i]);
+
+    onevec x(n);
+    rep(i, 0, n) in(x[i]);
+
+    // dp[i] は長さ i の系列における最小二乗和
+    matrix dp(n + 1, onevec(256, INFL));
+    dp[0][128] = 0;
+
+    // DP 遷移
+    rep(i, 1, n + 1) {
+      ll num = x[i - 1];
+      rep(j, 0, 256) {
+        if (dp[i - 1][j] == INFL)
+          continue;
+        rep(k, 0, m) {
+          ll newy = j + c[k];
+          if (newy > 255)
+            newy = 255;
+          if (newy < 0)
+            newy = 0;
+          ll diff = num - newy;
+          dp[i][newy] = min(dp[i][newy], dp[i - 1][j] + diff * diff);
+        }
+      }
+    }
+
+    // 最小コストを出力
+    ll ans = *min_element(all(dp[n]));
+    out(ans);
+  }
+  return 0;
+}
