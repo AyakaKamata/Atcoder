ac4cf362eea2331fd29ecb6853e079c497792759
0w0 <kamata.aya2019@gmail.com>
2025-02-12 23:03:20 +0100

    practice interval dp

---
+++ Codeforces/cf_1312_e.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rrep(i, a, b) for (ll i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define YN                                                                     \
+  {                                                                            \
+    cout << "Yes" << endl;                                                     \
+  }                                                                            \
+  else {                                                                       \
+    cout << "No" << endl;                                                      \
+  } // if(a==b)YN;
+#define fail cout << -1 << endl
+#pragma GCC optimize("-O3")
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using i128 = __int128_t;
+using pll = pair<ll, ll>;
+using pld = pair<ld, ld>;
+using matrix = vector<vector<ll>>;
+using matrixld = vector<vector<ld>>;
+using matrixb = vector<vector<bool>>;
+using onevec = vector<ll>;
+using onevecld = vector<ld>;
+using onevecb = vector<bool>;
+using lset = set<ll>;
+
+const ll INFL = 1LL << 60;
+template <class T> T chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+  }
+  return a;
+}
+template <class T> T chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+  }
+  return a;
+}
+template <typename T> ll lsize(const T &container) {
+  return static_cast<ll>(container.size());
+}
+template <typename T> bool ifbit(T number, ll bit) {
+  return (number & (1LL << bit)) != 0;
+}
+template <typename T> ll twice(const T &a) { return a * a; }
+template <typename T> T bits_count(T v) { return __builtin_popcountll(v); }
+
+template <typename T> T factorial(T n) {
+  T result = 1;
+  for (T i = 1; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
+}
+
+template <typename T> T pdistance(const vector<pair<T, T>> &xy, int i, int j) {
+  return sqrt(twice(xy[i].first - xy[j].first) +
+              twice(xy[i].second - xy[j].second));
+}
+
+template <typename T> void print(T value, int precision) {
+  cout << fixed << setprecision(precision) << value << endl;
+}
+
+template <typename... Args> void in(Args &...args) { (cin >> ... >> args); }
+
+template <typename... Args> void out(const Args &...args) {
+  constexpr size_t n = sizeof...(args);
+  size_t i = 0;
+  ((cout << args << (++i < n ? " " : "")), ...) << "\n";
+}
+
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, 1, -1, 1, -1};
+string strrep(const string &s, const string &from, const string &to) {
+
+  unordered_map<char, char> trans;
+  for (size_t i = 0; i < from.size(); ++i) {
+    trans[from[i]] = to[i];
+  }
+
+  string res = s;
+  for (char &c : res) {
+    if (trans.count(c)) {
+      c = trans[c];
+    }
+  }
+  return res;
+}
+template <typename T> bool isprime(T N) {
+  if (N < 2)
+    return false;
+  for (T i = 2; i * i <= N; i++) {
+    if (N % i == 0)
+      return false;
+  }
+  return true;
+}
+/*--------------------------------------------------------
+                         \0w0/
+                        OwOkaomoji
+                     ｡˚ (¦3ꇤ )3 ⋆｡˚✩
+----------------------------------------------------------*/
+
+ll n;
+onevec d;
+matrix dp;
+matrix mer;
+
+// 区間 [l, r] の最適解を求めるDP
+ll rec(ll l, ll r) {
+  ll &target = dp[l][r];
+  if (target != INFL)
+    return target;
+  if (l == r)
+    return 1;
+
+  // // 例: 区間を2つに分けて試す場合
+  for (ll i = l; i < r; ++i) {
+    chmin(target, rec(l, i) + rec(i + 1, r));
+  }
+  if (mer[l][r] != -1) {
+    chmin(target, 1LL);
+  }
+
+  return target;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  in(n);
+
+  d.resize(n);
+  rep(i, 0, n) in(d[i]);
+
+  dp.assign(n + 1, onevec(n + 1, INFL));
+  mer.assign(n, onevec(n, -1));
+  // Precompute which segments can be merged into a single value
+  rep(len, 1, n + 1) {
+    rep(l, 0, n - len + 1) {
+      ll r = l + len - 1;
+      if (len == 1) {
+        mer[l][r] = d[l];
+      } else {
+        rep(mid, l, r) {
+          if (mer[l][mid] != -1 && mer[l][mid] == mer[mid + 1][r]) {
+            mer[l][r] = mer[l][mid] + 1;
+            break;
+          }
+        }
+      }
+    }
+  }
+  out(rec(0, n - 1));
+
+  return 0;
+}
