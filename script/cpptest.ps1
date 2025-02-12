5c67d23ca2204a38cb893440588af9cba8203a61
0w0 <kamata.aya2019@gmail.com>
2025-02-12 23:03:31 +0100

    add interval dynamic programming snippet and improve URL generation in cpptest.ps1

--- script/cpptest.ps1
+++ script/cpptest.ps1
@@ -1,10 +1,10 @@
 $problem_name = $Args[0]
 $test_dir = "test/$problem_name"
 
-if (($problem_name -is [string] -and (($problem_name.Substring(0, 5) -eq "ALDS1" ) -or ($problem_name.Substring(0, 5) -eq "DPL_1" )))-or $problem_name -match '^\d') {
+if ($problem_name.Length -ge 5 -and ($problem_name.Substring(0, 5) -eq "ALDS1" -or $problem_name.Substring(0, 5) -eq "DPL_1") -or $problem_name -match '^\d') {
   $url = "https://onlinejudge.u-aizu.ac.jp/problems/$problem_name"
 } else {
-  $base_url = $problem_name.Replace("_", "-").Substring(0, $problem_name.Length - 2)
+  $base_url = if ($problem_name.Length -ge 2) { $problem_name.Replace("_", "-").Substring(0, $problem_name.Length - 2) } else { $problem_name.Replace("_", "-") }
 
   # 問題のURLを生成
   $url = "https://atcoder.jp/contests/$base_url/tasks/$problem_name"
@@ -15,11 +15,10 @@ if (! (Test-Path $test_dir)) {
   oj dl -d $test_dir $url  # "oj dl" コマンドでテストケースをダウンロード
 }
 
-
 # コンパイルとテスト実行
-if (($problem_name -is [string] -and (($problem_name.Substring(0, 5) -eq "ALDS1" ) -or ($problem_name.Substring(0, 5) -eq "DPL_1" )))-or $problem_name -match '^\d'){
+if ($problem_name.Length -ge 5 -and ($problem_name.Substring(0, 5) -eq "ALDS1" -or $problem_name.Substring(0, 5) -eq "DPL_1") -or $problem_name -match '^\d') {
   g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
-}else{
+} else {
   g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && oj s $url src/$problem_name.cpp && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
 }
 Remove-Item -Force -Path bin/* -ErrorAction SilentlyContinue
