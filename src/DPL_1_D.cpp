21005f82c6470a9e1e60f3e352b103daaebe56e7
0w0 <kamata.aya2019@gmail.com>
2025-02-25 13:00:42 +0100

    practice LIS

---
+++ src/DPL_1_D.cpp
@@ -0,0 +1,185 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rrep(i, a, b) for (ll i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define YN                                                                     \
+  {                                                                            \
+    cout << "Yes" << endl;                                                     \
+  }                                                                            \
+  else {                                                                       \
+    cout << "No" << endl;                                                      \
+  } // if(a==b)YN;
+#define FAIL cout << -1 << endl
+#pragma GCC optimize("-O3")
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using i128 = __int128_t;
+using pll = pair<ll, ll>;
+using pld = pair<ld, ld>;
+using matrix = vector<vector<ll>>;
+using matrixld = vector<vector<ld>>;
+using matrixb = vector<vector<bool>>;
+using onevec = vector<ll>;
+using onevecld = vector<ld>;
+using onevecb = vector<bool>;
+using lset = set<ll>;
+
+const ll INFL = 1LL << 60;
+template <class T> T chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+  }
+  return a;
+}
+template <class T> T chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+  }
+  return a;
+}
+template <typename T> ll lsize(const T &container) {
+  return static_cast<ll>(container.size());
+}
+template <typename T> bool ifbit(T number, ll bit) {
+  return (number & (1LL << bit)) != 0;
+}
+template <typename T> ll twice(const T &a) { return a * a; }
+template <typename T> T bits_count(T v) { return __builtin_popcountll(v); }
+
+template <typename T> T factorial(T n) {
+  T result = 1;
+  for (T i = 1; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
+}
+
+template <typename T> T pdistance(const vector<pair<T, T>> &xy, int i, int j) {
+  return sqrt(twice(xy[i].first - xy[j].first) +
+              twice(xy[i].second - xy[j].second));
+}
+template <typename T>
+int isperp(const vector<pair<T, T>> &xyz, int i, int j, int k) {
+  T dx1 = xyz[i].first - xyz[j].first, dy1 = xyz[i].second - xyz[j].second;
+  T dx2 = xyz[k].first - xyz[j].first, dy2 = xyz[k].second - xyz[j].second;
+
+  T dot = dx1 * dx2 + dy1 * dy2;
+
+  return (dot > 0) ? 0 : (dot == 0) ? 1 : -1;
+}
+
+template <typename T>
+T crossp(const vector<pair<T, T>> &xyz, int i, int j, int k) {
+  T dx1 = xyz[i].first - xyz[j].first, dy1 = xyz[i].second - xyz[j].second;
+  T dx2 = xyz[k].first - xyz[j].first, dy2 = xyz[k].second - xyz[j].second;
+
+  return dx1 * dy2 - dy1 * dx2;
+}
+
+template <typename T> T mod_exp(T base, T exp, T mod) {
+  T result = 1;
+  while (exp > 0) {
+    if (exp & 1)
+      result = (result * base) % mod;
+    base = (base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+template <typename T> void print(T value, int precision) {
+  cout << fixed << setprecision(precision) << value << endl;
+}
+
+template <typename... Args> void in(Args &...args) { (cin >> ... >> args); }
+
+template <typename... Args> void out(const Args &...args) {
+  constexpr size_t n = sizeof...(args);
+  size_t i = 0;
+  ((cout << args << (++i < n ? " " : "")), ...) << "\n";
+}
+
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, 1, -1, 1, -1};
+string strrep(const string &s, const string &from, const string &to) {
+
+  unordered_map<char, char> trans;
+  for (size_t i = 0; i < from.size(); ++i) {
+    trans[from[i]] = to[i];
+  }
+
+  string res = s;
+  for (char &c : res) {
+    if (trans.count(c)) {
+      c = trans[c];
+    }
+  }
+  return res;
+}
+template <typename T> bool isprime(T N) {
+  if (N < 2)
+    return false;
+  for (T i = 2; i * i <= N; i++) {
+    if (N % i == 0)
+      return false;
+  }
+  return true;
+}
+vector<pair<char, int>> rle(const string &s) {
+  vector<pair<char, int>> res;
+  for (int i = 0, cnt; i < s.size(); i += cnt) {
+    cnt = 1;
+    while (i + cnt < s.size() && s[i] == s[i + cnt])
+      cnt++;
+    res.emplace_back(s[i], cnt);
+  }
+  return res;
+}
+/*--------------------------------------------------------
+                         \0w0/
+                        OwOkaomoji
+                     ｡˚ (¦3ꇤ )3 ⋆｡˚✩
+----------------------------------------------------------*/
+// 関数: lisLength
+// 説明: 与えられた整数のシーケンスから最長増加部分列 (LIS) の長さを計算する
+// 引数: seq = 整数のベクトル
+// 戻り値: LIS の長さ (整数)
+ll lislen(const onevec &seq) {
+  // 空のシーケンスの場合は 0 を返す
+  if (seq.empty())
+    return 0;
+
+  onevec lis; // 現在のLISを格納するベクトル
+  lis.push_back(seq[0]);
+
+  // シーケンスの2番目の要素から処理を開始
+  rep(i, 1, lsize(seq)) {
+    // 現在の要素がlisの最後の要素より大きい場合、末尾に追加
+    if (seq[i] > lis.back()) {
+      lis.push_back(seq[i]);
+    } else {
+      // そうでない場合、lower_boundを用いてseq[i]が入るべき位置を探す
+      auto pos = lower_bound(lis.begin(), lis.end(), seq[i]);
+      *pos = seq[i]; // 該当位置の値を更新
+    }
+  }
+
+  // lisに格納された要素数がLISの長さになる
+  return lis.size();
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n;
+  in(n);
+  onevec a(n);
+  rep(i, 0, n) { in(a[i]); }
+  out(lislen(a));
+  return 0;
+}
