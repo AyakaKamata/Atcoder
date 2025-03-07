7bb827454b07fb37107a5935a4ad25d3c48b0edd
0w0 <kamata.aya2019@gmail.com>
2025-03-07 22:05:21 +0100

    practice Kruskal

---
+++ src/arc076_b.cpp
@@ -0,0 +1,264 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rrep(i, a, b) for (ll i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define YN                                                                     \
+  {                                                                            \
+    cout << "Yes" << endl;                                                     \
+  }                                                                            \
+  else {                                                                       \
+    cout << "No" << endl;                                                      \
+  } // if(a==b)YN;
+#define FAIL cout << -1 << endl
+#pragma GCC optimize("-O3")
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using i128 = __int128_t;
+using pll = pair<ll, ll>;
+using pld = pair<ld, ld>;
+using matrix = vector<vector<ll>>;
+using matrixld = vector<vector<ld>>;
+using matrixb = vector<vector<bool>>;
+using onevec = vector<ll>;
+using onevecld = vector<ld>;
+using onevecb = vector<bool>;
+using lset = set<ll>;
+
+const ll INFL = 1LL << 60;
+template <class T> T chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+  }
+  return a;
+}
+template <class T> T chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+  }
+  return a;
+}
+template <typename T> ll lsize(const T &container) {
+  return static_cast<ll>(container.size());
+}
+template <typename T> bool ifbit(T number, ll bit) {
+  return (number & (1LL << bit)) != 0;
+}
+template <typename T> T twice(const T &a) { return a * a; }
+template <typename T> T bits_count(T v) { return __builtin_popcountll(v); }
+
+template <typename T> T factorial(T n) {
+  T result = 1;
+  for (T i = 1; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
+}
+
+template <typename T> T pdistance(const vector<pair<T, T>> &xy, int i, int j) {
+  return sqrt(twice(xy[i].first - xy[j].first) +
+              twice(xy[i].second - xy[j].second));
+}
+template <typename T>
+int isperp(const vector<pair<T, T>> &xyz, int i, int j, int k) {
+  T dx1 = xyz[i].first - xyz[j].first, dy1 = xyz[i].second - xyz[j].second;
+  T dx2 = xyz[k].first - xyz[j].first, dy2 = xyz[k].second - xyz[j].second;
+
+  T dot = dx1 * dx2 + dy1 * dy2;
+
+  return (dot > 0) ? 0 : (dot == 0) ? 1 : -1;
+}
+
+template <typename T>
+T crossp(const vector<pair<T, T>> &xyz, int i, int j, int k) {
+  T dx1 = xyz[i].first - xyz[j].first, dy1 = xyz[i].second - xyz[j].second;
+  T dx2 = xyz[k].first - xyz[j].first, dy2 = xyz[k].second - xyz[j].second;
+
+  return dx1 * dy2 - dy1 * dx2;
+}
+
+template <typename T> T mod_exp(T base, T exp, T mod) {
+  T result = 1;
+  while (exp > 0) {
+    if (exp & 1)
+      result = (result * base) % mod;
+    base = (base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+template <typename T> void print(T value, int precision) {
+  cout << fixed << setprecision(precision) << value << endl;
+}
+
+template <typename... Args> void in(Args &...args) { (cin >> ... >> args); }
+
+template <typename... Args> void out(const Args &...args) {
+  constexpr size_t n = sizeof...(args);
+  size_t i = 0;
+  ((cout << args << (++i < n ? " " : "")), ...) << "\n";
+}
+
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, 1, -1, 1, -1};
+string strrep(const string &s, const string &from, const string &to) {
+
+  unordered_map<char, char> trans;
+  for (size_t i = 0; i < from.size(); ++i) {
+    trans[from[i]] = to[i];
+  }
+
+  string res = s;
+  for (char &c : res) {
+    if (trans.count(c)) {
+      c = trans[c];
+    }
+  }
+  return res;
+}
+template <typename T> bool isprime(T N) {
+  if (N < 2)
+    return false;
+  for (T i = 2; i * i <= N; i++) {
+    if (N % i == 0)
+      return false;
+  }
+  return true;
+}
+template <typename T>
+using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+vector<pair<char, int>> rle(const string &s) {
+  vector<pair<char, int>> res;
+  for (int i = 0, cnt; i < s.size(); i += cnt) {
+    cnt = 1;
+    while (i + cnt < s.size() && s[i] == s[i + cnt])
+      cnt++;
+    res.emplace_back(s[i], cnt);
+  }
+  return res;
+}
+/*--------------------------------------------------------
+                         \0w0/
+                        OwOkaomoji
+                     ｡˚ (¦3ꇤ )3 ⋆｡˚✩
+----------------------------------------------------------*/
+/* UnionFind：素集合系管理の構造体(union by rank)
+    isSame(x, y): x と y が同じ集合にいるか。 計算量はならし O(α(n))
+    unite(x, y): x と y を同じ集合にする。計算量はならし O(α(n))
+*/
+struct UnionFind { // The range of node number is 0 to n-1
+  vector<ll> rank, parents;
+  UnionFind() {}
+  UnionFind(ll n) { // make n trees.
+    rank.resize(n, 0);
+    parents.resize(n, 0);
+    rep(i, 0, n) { makeTree(i); }
+  }
+  void makeTree(ll x) {
+    parents[x] = x; // the parent of x is x
+    rank[x] = 0;
+  }
+  bool isSame(ll x, ll y) { return findRoot(x) == findRoot(y); }
+  void unite(ll x, ll y) {
+    x = findRoot(x);
+    y = findRoot(y);
+    if (rank[x] > rank[y]) {
+      parents[y] = x;
+    } else {
+      parents[x] = y;
+      if (rank[x] == rank[y]) {
+        rank[y]++;
+      }
+    }
+  }
+  int findRoot(ll x) {
+    if (x != parents[x])
+      parents[x] = findRoot(parents[x]);
+    return parents[x];
+  }
+};
+
+// 辺の定義
+struct Edge {
+  long long u;
+  long long v;
+  long long cost;
+};
+bool comp_e(const Edge &e1, const Edge &e2) {
+  return e1.cost < e2.cost;
+} // 辺を直接比較するための関数
+
+/* Kruskal :クラスカル法で minimum spanning tree を求める構造体
+    入力: 辺のvector, 頂点数V
+    最小全域木の重みの総和: sum
+    計算量: O(|E|log|V|)
+*/
+struct Kruskal {
+  UnionFind uft;
+  ll sum;  // 最小全域木の重みの総和
+  ll size; // 木の数
+  vector<Edge> edges;
+  ll V;
+  ll K;
+  Kruskal(const vector<Edge> &edges_, ll V_, ll K_)
+      : edges(edges_), V(V_), K(K_), size(V_) {
+    init();
+  }
+  void init() {
+    sort(edges.begin(), edges.end(), comp_e); // 辺の重みでソート
+    uft = UnionFind(V);
+    sum = 0;
+    for (auto e : edges) {
+      if (size == K) {
+        break;
+      }
+      if (!uft.isSame(e.u, e.v)) { // 閉路にならなければ加える
+        uft.unite(e.u, e.v);
+        sum += e.cost;
+        size--;
+      }
+    }
+  }
+};
+
+// グラフが隣接リストで与えられる時はクラスカル法
+int main() {
+
+  ll N; // 頂点の数、辺の数,木の数
+  in(N);
+  vector<pll> vx(N);
+  vector<pll> vy(N);
+  rep(i, 0, N) {
+    ll x, y;
+    in(x, y);
+    vx[i] = {x, i};
+    vy[i] = {y, i};
+  }
+  sort(all(vx));
+  sort(all(vy));
+
+  vector<Edge> edges;
+  rep(i, 0, N - 1) {
+    edges.push_back(
+        {vx[i].second, vx[i + 1].second, vx[i + 1].first - vx[i].first});
+    edges.push_back(
+        {vy[i].second, vy[i + 1].second, vy[i + 1].first - vy[i].first});
+  }
+
+  // rep(i, 0, M) {
+  //   ll s, t, w; // start, goal, weight
+  //   in(s, t, w);
+  //   s--;
+  //   t--;
+  //   Edge e = {s, t, w};
+  //   edges[i] = e;
+  // }
+  Kruskal krs(edges, N, 1);
+  out(krs.sum);
+  return 0;
+}
