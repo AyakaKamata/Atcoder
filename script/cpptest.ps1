d651cb1ea89ae5949b7d2a341190d3405905c7ad
0w0 <kamata.aya2019@gmail.com>
2025-03-07 22:06:42 +0100

    add support for NTL problem type in cpptest.ps1

--- script/cpptest.ps1
+++ script/cpptest.ps1
@@ -1,7 +1,7 @@
 $problem_name = $Args[0]
 $test_dir = "test/$problem_name"
 
-if ($problem_name.Length -ge 5 -and ($problem_name.Substring(0, 5) -eq "ALDS1" -or $problem_name.Substring(0, 3) -eq "DPL"-or $problem_name.Substring(0, 3) -eq "GRL") -or $problem_name -match '^\d') {
+if ($problem_name.Length -ge 5 -and ($problem_name.Substring(0, 5) -eq "ALDS1" -or $problem_name.Substring(0, 3) -eq "DPL"-or $problem_name.Substring(0, 3) -eq "GRL"-or $problem_name.Substring(0, 3) -eq "NTL") -or $problem_name -match '^\d') {
   $url = "https://onlinejudge.u-aizu.ac.jp/problems/$problem_name"
 } else {
   $base_url = if ($problem_name.Length -ge 2) { $problem_name.Replace("_", "-").Substring(0, $problem_name.Length - 2) } else { $problem_name.Replace("_", "-") }
@@ -16,7 +16,7 @@ if (! (Test-Path $test_dir)) {
 }
 
 # コンパイルとテスト実行
-if ($problem_name.Length -ge 5 -and ($problem_name.Substring(0, 5) -eq "ALDS1" -or $problem_name.Substring(0, 3) -eq "DPL" -or $problem_name.Substring(0, 3) -eq "GRL") -or $problem_name -match '^\d') {
+if ($problem_name.Length -ge 5 -and ($problem_name.Substring(0, 5) -eq "ALDS1" -or $problem_name.Substring(0, 3) -eq "DPL" -or $problem_name.Substring(0, 3) -eq "GRL"-or $problem_name.Substring(0, 3) -eq "NTL") -or $problem_name -match '^\d') {
   g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
 } else {
   g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && oj s $url src/$problem_name.cpp && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
