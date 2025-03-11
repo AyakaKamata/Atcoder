ef32d0ecb95cb0b36772d9c50b4782a8130f4030
0w0 <kamata.aya2019@gmail.com>
2025-03-11 01:39:01 +0100

    add ac library

--- script/cpptest.ps1
+++ script/cpptest.ps1
@@ -17,8 +17,8 @@ if (! (Test-Path $test_dir)) {
 
 # コンパイルとテスト実行
 if ($problem_name.Length -ge 5 -and ($problem_name.Substring(0, 5) -eq "ALDS1" -or $problem_name.Substring(0, 3) -eq "DPL" -or $problem_name.Substring(0, 3) -eq "GRL"-or $problem_name.Substring(0, 3) -eq "NTL") -or $problem_name -match '^\d') {
-  g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
+  g++ src/$problem_name.cpp -std=c++17 -I ../ac-library -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
 } else {
-  g++ src/$problem_name.cpp -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && oj s $url src/$problem_name.cpp && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
+  g++ src/$problem_name.cpp  -std=c++17 -I ../ac-library -o bin/$problem_name.out && oj test -c bin/$problem_name.out -d $test_dir && oj s $url src/$problem_name.cpp && Remove-Item -Force -Path $test_dir -ErrorAction SilentlyContinue
 }
 Remove-Item -Force -Path bin/* -ErrorAction SilentlyContinue
